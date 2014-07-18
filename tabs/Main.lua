61d98c6ea81631408b21d47231be9d3ffa63e95b
denisik at codea-scm <denisik@codea-scm.aws.mapote.com>
2014-07-18 17:58:22 -0400

    fixes and colors

--- tabs/Main.lua
+++ tabs/Main.lua
@@ -1,30 +1,33 @@
 -- Gist
 
 function setup()
-    url = ""
+    url, c = "", color(203, 209, 60, 255)
     parameter.action("Paste gist url", function()
-        url = pasterboard.text
+        url = pasteboard.text
         parameter.action("Download", function()
             http.request(url, function(data)
                 saveProjectTab("Main", data)
                 msg = "Success!"
+                c = color(96, 181, 47, 255)
                 parameter.action("Quit", function()
                     close()
                 end)
             end, function(err)
                 msg = err
+                c = color(177, 49, 49, 255)
                 parameter.action("Quit", function()
                     close()
                 end)
             end)
+        end)
     end)
 end
 
 function draw()
-    background(0, 0, 0)
-    fill(128, 128, 128)
+    background(18, 18, 19, 255)
+    fill(c)
     textWrapWidth(WIDTH-100)
-    fontSize(40)
+    fontSize(35)
     if msg then
         text(msg, WIDTH/2, HEIGHT/2)
     else
