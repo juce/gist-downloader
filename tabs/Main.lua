552a746fb713f30c985df70eda5e538119c01a00
denisik at codea-scm <denisik@codea-scm.aws.mapote.com>
2014-07-19 04:31:54 -0400

    adding tab-splitting

--- tabs/Main.lua
+++ tabs/Main.lua
@@ -1,4 +1,25 @@
--- Gist
+-- Gist downloader
+
+local function iter(obj)
+    if not obj.name then
+        return
+    end
+    local data = obj.data
+    local name, pos = obj.name, obj.pos
+    local s, e, next = data:find("\n[-][-]# ([%w_]+)[^\n]*\n.", pos)
+    obj.name, obj.pos = next, e
+    return name, data:sub(pos, s)
+end
+
+function tabs(data)
+    local s, e, name = data:find("^%s*[-][-]# ([%w_]+)[^\n]*\n.")
+    local obj = {
+        pos = e or 1, 
+        name = name or "Main",
+        data = data,
+    }
+    return iter, obj
+end
 
 function setup()
     url, c = "", color(203, 209, 60, 255)
@@ -9,10 +30,15 @@ function setup()
                 url = url .. "/raw"
             end
             http.request(url, function(data)
-                saveProjectTab("Main", data)
-                msg, c = "Success!", color(96, 181, 47, 255)
-                parameter.action("Quit", function()
-                    close()
+                msg = "Downloaded. Splitting into tabs ..."
+                tween.delay(1, function()
+                    for tabname,tabdata in tabs(data) do
+                        saveProjectTab(tabname, tabdata)
+                    end
+                    msg, c = "Success!", color(96, 181, 47, 255)
+                    parameter.action("Quit", function()
+                        close()
+                    end)
                 end)
             end, function(err)
                 msg, c = err, color(177, 49, 49, 255)
